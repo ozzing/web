9985b378e371787ecefa9f30158fd6d87c00ef0d
주어진사랑 <63996052+ozzing@users.noreply.github.com>
2021-07-20 02:44:44 +0900

    nodejs
    ~UI

--- nodejs/main.js
+++ nodejs/main.js
@@ -2,65 +2,77 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title, list, body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    <a href="/create">CREATE</a>
+    ${body}
+  </body>
+  </html>
+  `;
+}
+function templateList(filelist){
+  var list = '<ol>';
+  var i=0;
+  while(i < filelist.length){
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+  }
+  i=0;
+  list = list + '</ol>';
+  return list;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
 
     if(pathname === '/'){
-      if(queryData.id === undefined){
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-            var title = 'Welcome';
-            var description = "Hello, Node.js!";
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              <ol>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=JavaScript">JavaScript</a></li>
-              </ol>
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-            `;
-            response.writeHead(200);
-            response.end(template);
-          });
+        if(queryData.id === undefined){
+            fs.readdir('./data', function(error, filelist){
+              var title = 'Welcome!';
+              var description = "Hello, Node.js!";
+              var list = templateList(filelist);
+              var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+              response.writeHead(200);
+              response.end(template);
+            });
         }
         else {
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-          var title = queryData.id;
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ol>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
-          response.writeHead(200);
-          response.end(template);
-        });
+          fs.readdir('./data', function(error, filelist){
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description){
+              var title = queryData.id;
+              var list = templateList(filelist);
+              var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+              response.writeHead(200);
+              response.end(template);
+            });
+          });
       }
+    }
+    else if (pathname === '/create'){
+      fs.readdir('./data', function(error, filelist){
+        var title = 'WEB - CREATE';
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `
+          <form action="http://localhost:3000/process_create" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p><textarea name="desription" placeholder="description"></textarea></p>
+            <p><input type="submit"></p>
+          </form>
+          `);
+        response.writeHead(200);
+        response.end(template);
+      });
     }
     else {
       response.writeHead(404);
